703f332aced68f3518082d7435ec131499ed18ed
Andrey Castro <Andrey.X.Carvajal.-ND@disney.com>
2015-11-12 07:22:48 -0600

    [Home] Reminders test implementation

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,10 @@ class IndexController extends Base_Controller_BaseController
     public function indexAction()
     {
         $this->view->title = 'Home';
+        $this->view->oilChanges = 0;
+        $this->view->severance = 1;
+        $this->view->dailyLoans = 3;
+        $this->view->unattendedClients = 2;
         //$UserModel = new Application_Model_UserModel();
         //$testModel->sayHello();
     }
