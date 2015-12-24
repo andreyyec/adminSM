1c19abf25b375442f9565018a7a70e1d88c333db
Andrey Castro <Andrey.X.Carvajal.-ND@disney.com>
2015-12-24 16:37:52 -0600

    [DB] Schema update

--- library/Base/SQL/adminsm_db.sql
+++ library/Base/SQL/adminsm_db.sql
@@ -1,3 +1,16 @@
+CREATE TABLE adminsm_policies(
+id SERIAL PRIMARY KEY,
+namePolicy VARCHAR(40) NOT NULL,
+numberPolicy INTEGER NOT NULL UNIQUE,
+employerIDType CHAR(1),
+employerID VARCHAR(25) NOT NULL UNIQUE,
+employerName VARCHAR(80),
+employerPhone INTEGER NOT NULL,
+employerFax INTEGER,
+employerMail VARCHAR(80) NOT NULL,
+employerAddress VARCHAR(250) NOT NULL
+);
+
 CREATE TABLE adminsm_employees(
 id SERIAL PRIMARY KEY,
 name VARCHAR(40) NOT NULL,
@@ -13,23 +26,8 @@ revenue INTEGER NOT NULL,
 daysamonth INTEGER NOT NULL,
 hoursamonth INTEGER NOT NULL,
 ocupation VARCHAR(80) NOT NULL,
-policy INT NOT NULL, 
+policy INT NOT NULL REFERENCES adminsm_policies(id), 
 workday VARCHAR(80) NOT NULL,
 active CHAR(1) NOT NULL,
 isemployer CHAR(1) NOT NULL
 );
-
-CREATE TABLE adminsm_policies(
-id SERIAL PRIMARY KEY,
-namePolicy VARCHAR(40) NOT NULL,
-numberPolicy INTEGER NOT NULL UNIQUE,
-employerIDType CHAR(1),
-employerID VARCHAR(25) NOT NULL UNIQUE,
-employerName VARCHAR(80),
-employerPhone INTEGER NOT NULL,
-employerFax INTEGER,
-employerMail VARCHAR(80) NOT NULL,
-employerAddress VARCHAR(250) NOT NULL
-);
-
-
